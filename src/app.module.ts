41de7efe24b151717196878f23e59bc3d0c09a3f
Harry Nguyen <hainguyen27798@gmail.com>
2024-04-24 18:00:17 +0700

    Update GitHub action and example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
 import { TypesenseNestModule } from '@app/typesense-nestjs-lib';
+import { Module } from '@nestjs/common';
 
 @Module({
     imports: [
         TypesenseNestModule.forRootSync({
             useFactory: () => ({
-                apiKey: "",
-                us
+                nodes: [
+                    {
+                        host: 'localhost',
+                        port: 8108,
+                        protocol: 'http',
+                    },
+                ],
+                apiKey: 'your typesense api key',
+                logLevel: 'trace',
+                retryIntervalSeconds: 2,
+                timeoutSeconds: 4,
             }),
         }),
     ],
